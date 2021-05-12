471f8ac1dc0688df5b35002735c01b7c9afe8157
pai-plznw4me <plznw4me@publicai.co.kr>
2021-05-12 21:47:43 +0900

    encoder 생성

--- conv_autoencoder.py
+++ conv_autoencoder.py
@@ -11,3 +11,32 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 import numpy as np
+
+
+class ConvAutoencoder:
+    def build(self, height, width, channel):
+        # 입력 레이어 생성
+        input_ = Input(shape=(height, width, channel))
+
+        # 히든 레이어 1 생성
+        layer = Conv2D(filters=32, kernel_size=(3, 3), strides=2, padding='same')(input_)
+        layer = LeakyReLU(alpha=0.2)(layer)
+        layer = BatchNormalization(axis=-1)(layer)
+
+        # 히든 레이어 2 생성
+        layer = Conv2D(filters=32, kernel_size=(3, 3), strides=2, padding='same')(layer)
+        layer = LeakyReLU(alpha=0.2)(layer)
+        layer = BatchNormalization(axis=-1)(layer)
+
+        #
+        layer = Flatten()(layer)
+        layer = Dense(units=16)(layer)
+        code = layer
+
+        model = Model(input_, code)
+        return model
+
+
+if __name__ == '__main__':
+    conv_ae = ConvAutoencoder()
+    model = conv_ae.build(200, 100, 3)
