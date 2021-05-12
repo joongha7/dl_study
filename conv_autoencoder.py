4115ff34262f7d7710dd0b29dff28d2ff6b7c605
pai-plznw4me <plznw4me@publicai.co.kr>
2021-05-12 21:08:31 +0900

    readme 파일 추가

---
+++ conv_autoencoder.py
@@ -0,0 +1,13 @@
+# import the necessary packages
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+import numpy as np
