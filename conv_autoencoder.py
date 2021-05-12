2893821ac041375e11259789d11434388bee0c83
박중하 <j.h.park@lginnotek.com>
2021-05-12 21:47:52 +0900

    Encoder 생성

--- conv_autoencoder.py
+++ conv_autoencoder.py
@@ -12,3 +12,43 @@ from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 import numpy as np
 
+
+class ConvAutoencoder:
+    def build(self, height, width, channel):
+        # 입력 레이어 생성
+        input_ = Input(shape=(height, width, channel))
+        print(input_)
+
+
+        # 히든 레이어 1 생성
+        layer = Conv2D(filters=32, kernel_size=(3,3), strides=2, padding='same')(input_)
+        layer = LeakyReLU(alpha= 0.2)(layer)
+        chanDim = layer.get_shape()[-1]
+        layer = BatchNormalization(axis=-1)(layer)
+        pass
+
+
+        # 히든 레이어 2 생성
+        layer = Conv2D(filters=32, kernel_size=(3,3), strides=2, padding='same')(layer)
+        layer = LeakyReLU(alpha= 0.2)(layer)
+        chanDim = layer.get_shape()[-1]
+        layer = BatchNormalization(axis=-1)(layer)
+
+
+        # Activation function 이 안들어가 있음
+        layer = Flatten()(layer)
+        layer = Dense(units = 16)(layer)
+        code = layer
+
+        # 모델 생성
+        # layer 들을 묶어서 모델 생성
+        # 시퀀스 구조
+        # 레이어 들이 묶여있는 집합을 모델이라고 한다.
+
+        model = Model(input_, code)
+
+        return model
+
+if __name__ == '__main__':
+    conv_ae = ConvAutoencoder()
+    model = conv_ae.build(200, 100, 3)
